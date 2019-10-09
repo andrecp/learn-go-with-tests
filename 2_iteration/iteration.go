6203d33da8950f7f2c1a3616a6700552474e6f89
Andre Prado <andreprado88@gmail.com>
2019-10-08 21:37:29 -0700

    Add iteration with Benchmark

---
+++ 2_iteration/iteration.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Repeat repeats a character, duh!
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated += character
+	}
+	return repeated
+}
