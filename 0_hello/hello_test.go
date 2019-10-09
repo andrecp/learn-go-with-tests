b4ff9987957b59b74f74d5cd2f2687927eb11c73
Andre Prado <andreprado88@gmail.com>
2019-10-08 20:53:18 -0700

    Refactor our tests, t.Helper excludes the function from stack traces

--- 0_hello/hello_test.go
+++ 0_hello/hello_test.go
@@ -3,22 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
 
 	t.Run("Saying hello to people", func(t *testing.T) {
 		got := hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
 		got := hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
