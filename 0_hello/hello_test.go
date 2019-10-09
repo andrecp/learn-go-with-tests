875316e4d1d4d306afa1ec0abf182bfce18425ca
Andre Prado <andreprado88@gmail.com>
2019-10-08 20:50:34 -0700

    Add subtest to test empty string use case

--- 0_hello/hello_test.go
+++ 0_hello/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	t.Run("Saying hello to people", func(t *testing.T) {
+		got := hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("Saying 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
