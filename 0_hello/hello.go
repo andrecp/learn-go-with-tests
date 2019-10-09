612e280b53c1df3d5e9255c6636c1a2e98670b81
Andre Prado <andreprado88@gmail.com>
2019-10-08 20:43:22 -0700

    Hello world now accepts a parameter

--- 0_hello/hello.go
+++ 0_hello/hello.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, world"
+func hello(name string) string {
+	return "Hello, " + name
 
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(hello("world"))
 }
